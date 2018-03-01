6db8c038bbc3be63ef201a75ce4eebf9f94fe784
Hossein Jazayeri <hossein@sam-media.com>
2018-03-01 10:09:09 +0100

    pass DataFrame to python app

--- reports/anomaly-detection/forecast_and_detect_anomalies.py
+++ reports/anomaly-detection/forecast_and_detect_anomalies.py
@@ -1,8 +1,10 @@
 from json import dumps
 from sys import stdin
 
+import pandas as pd
 from sam_anomaly_detector import Detector
 
 raw_data = stdin.readlines()[0]
-anomalies = Detector().find_all_anomalies(dataset=raw_data)
-print(dumps(anomalies))
+data = pd.read_json(raw_data, orient='records')
+anomalies = Detector(min_time_points=0, min_dataset_size=0).forecast_today(dataset=data)
+print(dumps(anomalies.to_json(orient='records')))
