24d410f9fda1af640aa297175a0a7fd29118196a
Hossein Jazayeri <hossein@sam-media.com>
2018-04-06 13:55:41 +0200

    duplicate presentation files for py integration

---
+++ reports/changed-affiliates/presentation_ml.tsx
@@ -0,0 +1,190 @@
+import * as React from "react";
+import {
+    A,
+    makeColumn,
+    negativeColorScale,
+    neutralColorScale,
+    newMakeUrl,
+    positiveColorScale,
+    TABLE,
+    THEAD
+} from '../presentation-utils-ml'
+import d3Format = require('d3-format');
+
+const R = require('ramda');
+
+export default function (results: any, params: any, {affiliatesMap}) {
+
+    let cqFormat = d3Format.format('0.0%');
+    let crFormat = d3Format.format('0.1%');
+    let intFormat = d3Format.format(',.0f');
+    let bigIntFormat = d3Format.format(',.2s');
+    let cpaFormat = d3Format.format(',.1f');
+    let rateFormat = R.pipe(d3Format.format(',.1f'), x => `${x}×`);
+
+    let metricToLabel = metric => ({
+        'views': 'Views',
+        'sales': 'Sales',
+        'active24': 'Active 24',
+        'cr': 'CR',
+        'cq': 'CQ',
+        'resubs': 'Re-Subs',
+        'releads': 'Re-Leads',
+        'pixels_ratio': 'Pixels',
+        'ecpa': 'eCPA',
+        'total_optouts': 'Unsubs'
+    })[metric] || metric;
+
+    let column = makeColumn(metricToLabel);
+
+    let {dateFrom, dateTo} = params;
+
+    let isHourly = params.frequency == 'hourly';
+
+    let {makeUrl, makeCountrySummaryUrl, makeAffiliateSummaryUrl} = newMakeUrl({dateFrom, dateTo});
+
+    const columns = [
+        column('views', positiveColorScale, bigIntFormat),
+        column('sales', positiveColorScale, intFormat),
+        column('cr', positiveColorScale, crFormat),
+        column('cq', positiveColorScale, cqFormat),
+        column('resubs', negativeColorScale, rateFormat),
+        column('releads', negativeColorScale, rateFormat),
+        column('active24', positiveColorScale, cqFormat),
+        column('pixels_ratio', neutralColorScale, cqFormat),
+        column('ecpa', negativeColorScale, cpaFormat)
+    ];
+
+    let topAffiliatesTable = (r) => {
+        return <TABLE>
+            <colgroup>
+                <col span="1" style={{width: '5%'}}/>
+                <col span="1" style={{width: '10%'}}/>
+            </colgroup>
+            {
+                columns.map(c => !!c.colgroup ? c.colgroup() : [])
+            }
+            <THEAD style={{backgroundColor: '#7f7f7f'}}>{[<th style={{paddingLeft: '0.3em'}} colSpan="2"><A
+                style={{color: 'white'}} href={makeCountrySummaryUrl(r.page)}>{r.page}</A></th>]
+                .concat(columns.map(c => c.th()))}</THEAD>
+            <tbody>
+            {r.sections.map(s =>
+                <tr style={{borderBottom: 'solid 1px silver'}}>
+                    <td style={{paddingLeft: '0.3em'}}><A href={makeAffiliateSummaryUrl(r.page, s.section)}>{r.page}</A>
+                    </td>
+                    <td><A href={makeAffiliateSummaryUrl(r.page, s.section)}>{affiliatesMap[s.section] || 'Unknown'}</A>
+                    </td>
+                    {
+                        columns.map(c => c.td(s))
+                    }
+                </tr>
+            )}
+            </tbody>
+            <tfoot>
+            <tr style={{borderBottom: 'solid 2px silver', fontWeight: 'bold', height: '5ex'}}>
+                <td colSpan="2" style={{paddingLeft: '0.3em'}}><A href={makeCountrySummaryUrl(r.page)}>{r.page}</A></td>
+                {
+                    columns.map(c => c.td(r))
+                }
+            </tr>
+            </tfoot>
+        </TABLE>;
+    };
+
+    // Top Changed Affiliates
+    let pagesColumns = (isHourly ? [column('views', positiveColorScale, bigIntFormat)] : [])
+        .concat([
+            column('sales', positiveColorScale, intFormat)
+            , column('cr', positiveColorScale, crFormat)
+            , column('cq', positiveColorScale, cqFormat)
+            , column('resubs', negativeColorScale, rateFormat)
+            , column('releads', negativeColorScale, rateFormat)
+            , column('active24', positiveColorScale, cqFormat)
+            , column('pixels_ratio', neutralColorScale, cqFormat)
+        ])
+        .concat(!isHourly ? [column('ecpa', negativeColorScale, cpaFormat)] : []);
+
+    let topChangedCountriesColumns = [column('views', positiveColorScale, bigIntFormat)]
+        .concat(pagesColumns)
+        .concat([column('total_optouts', negativeColorScale, intFormat)]);
+
+    let topChangedCountries =
+        <TABLE>
+            <colgroup>
+                <col span="1" style={{width: '5%'}}/>
+            </colgroup>
+            {topChangedCountriesColumns.map(c => !!c.colgroup ? c.colgroup() : [])}
+            <THEAD style={{backgroundColor: '#1f77b4'}}>
+            {[<th></th>].concat(topChangedCountriesColumns.map(c => c.th()))}
+            </THEAD>
+            <tbody>
+            {results.map(s =>
+                <tr style={{borderBottom: 'solid 1px silver'}}>
+                    <td style={{paddingLeft: '0.3em'}}>
+                        <A href={makeCountrySummaryUrl(s.page)}>
+                            {s.page}
+                        </A>
+                    </td>
+                    {topChangedCountriesColumns.map(c => c.td(s))}
+                </tr>
+            )}
+            </tbody>
+        </TABLE>;
+
+    let topAffiliates = () => {
+        return results.map((r, i) =>
+            <div style={{marginTop: `${i > 0 ? 1 : 0}em`}}>
+                {topAffiliatesTable(r)}
+            </div>);
+    };
+
+    let topChangedAffiliates = () => {
+        return <TABLE>
+            <colgroup>
+                <col span="1" style={{width: '5%'}}/>
+                <col span="1" style={{width: '10%'}}/>
+            </colgroup>
+            {pagesColumns.map(c => !!c.colgroup ? c.colgroup() : [])}
+            <THEAD style={{backgroundColor: '#8c564b'}}>{[<th></th>,
+                <th>Affiliate</th>].concat(pagesColumns.map(c => c.th()))}</THEAD>
+            <tbody>
+            {R.pipe(
+                R.chain(p => p.sections.map(s => R.merge(s, {page: p.page}))),
+                R.reduce(({list, page}, a) => ({
+                    list: list.concat([R.merge(a, {isNew: a.page != page})]),
+                    page: a.page
+                }), {list: [], page: null}),
+                R.prop('list')
+            )(results).map(s =>
+                <tr style={R.merge({borderBottom: 'solid 1px silver'}, s.isNew ? {borderTop: 'solid 2px #7f7f7f'} : {})}>
+                    <td style={{paddingLeft: '0.3em'}}>
+                        <A target="_blank" href={makeCountrySummaryUrl(s.page)}>{s.page}</A>
+                    </td>
+                    <td style={{paddingLeft: '0.3em'}}>
+                        <A target="_blank" href={makeAffiliateSummaryUrl(s.page, s.section)} title={s.section}>
+                            {affiliatesMap[s.section] || s.section}
+                        </A>
+                    </td>
+                    {pagesColumns.map(c => c.td(s, {ignoreBgColor: !['sales', 'pixels_ratio', 'releads'].some(v => v == c.value)}))}
+                </tr>
+            )}
+            </tbody>
+        </TABLE>;
+    };
+
+    let MakeChange = (title, component) => {
+        return <div>
+            <h3>{title}</h3>
+            <div>{component}</div>
+        </div>;
+    };
+
+    return {
+        changedCountries: MakeChange('Top Changed Countries', topChangedCountries),
+        changedAffiliates: '',
+        topAffiliates: '',
+
+        // changedAffiliates: MakeChange('Top Changed Affiliates', topChangedAffiliates()),
+        // topAffiliates: MakeChange('Top Affiliates', topAffiliates())
+    };
+}
---
+++ reports/presentation-utils-ml.tsx
@@ -0,0 +1,131 @@
+import * as React from "react";
+import d3Format = require('d3-format');
+
+const d3Scale = require('d3-scale');
+const R = require('ramda');
+
+export const A = ({href, children, style}) => <a style={R.merge({color: 'black'}, style)} href={href}>{children}</a>;
+
+export const TABLE = ({children}) => <table width="100%" style={{
+    backgroundColor: 'white',
+    tableLayout: 'fixed',
+    minWidth: '1000px',
+    maxWidth: '1200px',
+    borderCollapse: 'collapse'
+}}>
+    {children}
+</table>;
+
+export const THEAD = ({children, style}) => <thead>
+<tr style={R.merge({color: '#eee', textAlign: 'left', backgroundColor: 'black', height: '4ex'}, style)}>
+    {children}
+</tr>
+</thead>;
+
+export const newColumn = (value, cols) => ({
+    value,
+    td: (s, options?) => R.pipe(
+        R.chain(c => c.cols),
+        R.map(c => <td style={{height: '4ex'}} title={c.title(s)}>{c.content(s, options)}</td>)
+    )(cols),
+    th: () => cols.map(c => <th colSpan={c.cols.length}>{c.label}</th>),
+    colgroup: () => <colgroup>
+        {R.pipe(R.chain(c => c.cols), R.addIndex(R.map)((c, i) => <col span="1" style={c.style}/>))(cols)}
+    </colgroup>
+});
+
+const setTitle = (metric) => s => {
+    if (
+        s.metrics != undefined &&
+        s.metrics[metric] != undefined &&
+        s.metrics[metric].stdChange != undefined &&
+        s.metrics[metric].change != undefined
+    ) {
+        let stdChange = s.metrics[metric].stdChange;
+        let change = s.metrics[metric].change;
+        return `
+Change: ${d3Format.format('0.1f')(stdChange)} σ
+Change: ${d3Format.format('0.0%')(change)}`
+    }
+    return '-';
+};
+
+export const makeColumn = metricToLabel => (metric, scale, format) => {
+    return newColumn(
+        metric,
+        [
+            {
+                label: metricToLabel(metric),
+                cols: [
+                    {
+                        style: {width: '10%'},
+                        title: setTitle(metric),
+                        content: (s, options) => ChangeSymbol(scale)(
+                            s.metrics.hasOwnProperty(metric) ? s.metrics[metric].stdChange : '',
+                            s.metrics.hasOwnProperty(metric) ? s.metrics[metric].change : '',
+                            s.metrics.hasOwnProperty(metric) ? format(s.metrics[metric].actual) : '',
+                            options
+                        )
+                    }
+                ]
+            }
+        ]
+    );
+};
+
+export const newMakeUrl = ({dateFrom, dateTo}) => {
+    /* TODO: links here should follow this format to allow authentication:
+     http://sigma.sam-media.com/daily_reports_archive/${yesterday}/?username=${username}&exp_ts=${tomorrow}&hash=${signature(username,tomorrow)(process.env.secret)}
+
+     the signature function can be imported from ./hash.ts
+     username can be passed as props (best case scenario)
+    */
+    const makeUrl = (filter, breakdown) => `http://sigma.sam-media.com/filter_page_section_row/${dateFrom}/${dateTo}/${filter}/${breakdown}/`
+    return {
+        makeUrl,
+        makeCountrySummaryUrl: (country_code) => makeUrl(`country_code=${country_code}`, `-/-/day`),
+        makeAffiliateSummaryUrl: (country_code, affiliate_id) => makeUrl(`country_code=${country_code},affiliate_id=${affiliate_id}`, `-/-/day`),
+        makeHandleSummaryUrl: (country_code, handle_name) => makeUrl(`country_code=${country_code},handle_name=${handle_name}`, `-/-/day`)
+    }
+};
+
+export const {ChangeSymbol, positiveColorScale, negativeColorScale, neutralColorScale} = (function () {
+    let colorScale = (domain, colors) => v => v == 0 ? 'white' : d3Scale.scaleQuantize().domain(domain).range(colors)(Math.abs(v));
+
+    let greens = colorScale([1, 5], ['#CBE7C1', '#BBDFB3', '#A5D19C', '#77BD65', '#54AE3D']);
+    let reds = colorScale([1, 5], ['#F7BBBB', '#F7A5A9', '#F4777D', '#EE4B4C', '#E22124']);
+    let positiveColorScale = v => v < 0 ? reds(v) : greens(v);
+    let negativeColorScale = v => v < 0 ? greens(v) : reds(v);
+    let neutralColorScale = colorScale(R.range(2, 7), ['#FAD1BD', '#FFCAAF', '#FAA27C', '#EF8656', '#FB6123']);
+
+    let ChangeSymbolSpan = ({style, children}) => {
+        let styles = R.merge({
+            display: 'inline-block'
+            , overflow: 'hidden'
+            , minWidth: '4em'
+            , maxWidth: '100%'
+            , float: 'left'
+            , padding: '0.15em 0em'
+        }, style);
+        return <span style={styles}>&nbsp;{children}&nbsp;</span>;
+    };
+
+    let ChangeSymbol = (scale) => {
+        return (stdChange?, change?, content?, options = {ignoreBgColor: false}) => {
+            let bgColor = options.ignoreBgColor ? '' : scale(stdChange);
+            let textColor = 'black';
+            if (bgColor == '#54AE3D' || bgColor == '#77BD65' || bgColor == '#E22124' || bgColor == '#EE4B4C') {
+                textColor = 'white';
+            }
+
+            return Math.abs(stdChange) < 1 || Math.abs(change) < 0.075 ?
+                <ChangeSymbolSpan>{content}</ChangeSymbolSpan> :
+                <ChangeSymbolSpan style={{
+                    backgroundColor: bgColor,
+                    color: textColor,
+                    borderRadius: '0.5em'
+                }}>{content}</ChangeSymbolSpan>
+        };
+    };
+    return {ChangeSymbol, positiveColorScale, negativeColorScale, neutralColorScale}
+})();
