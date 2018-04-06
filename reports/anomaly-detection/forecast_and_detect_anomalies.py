70c911184adba1cb0eb414c28efed76379fbd8f7
Hossein Jazayeri <hossein@sam-media.com>
2018-04-06 13:53:18 +0200

    fix time

--- reports/anomaly-detection/forecast_and_detect_anomalies.py
+++ reports/anomaly-detection/forecast_and_detect_anomalies.py
@@ -6,5 +6,10 @@ from sam_anomaly_detector import Detector
 
 raw_data = stdin.readlines()[0]
 data = pd.read_json(raw_data, orient='records')
+data['time'] = pd.to_datetime(data['time']).apply(lambda d: d.strftime('%Y-%m-%d'))
 anomalies = Detector(min_time_points=0, min_dataset_size=0).forecast_today(dataset=data)
-print(dumps(anomalies.to_json(orient='records')))
+columns = []
+if not anomalies.empty:
+    columns = ['ds', 'actual', 'yhat_lower', 'yhat', 'yhat_upper', 'change', 'stdChange']
+    anomalies['ds'] = pd.to_datetime(anomalies['ds'], unit='ms').apply(lambda ds: ds.strftime('%Y-%m-%d'))
+print(dumps(anomalies[columns].to_json(orient='records')))
